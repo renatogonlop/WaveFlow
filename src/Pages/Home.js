f72a6392469aa634f333b51750cfc61296bbc8d5
Renato Gonçalves <88465590+renatogonlop@users.noreply.github.com>
2022-03-28 20:51:40 -0300

    Add files via upload

---
+++ src/Pages/Home.js
@@ -0,0 +1,50 @@
+import Carousel from 'react-bootstrap/Carousel'
+
+function Home() {
+    return (
+        // <img src="img_2.png" alt="SING!" class="d-block w-100"/>
+
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="img_1.png"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="img_2.png"
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="img_3.png"
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+
+
+
+    )
+
+}
+export default Home;
