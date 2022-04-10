26b8ebf89133b305599211458fcfb87b1f0d1b66
Matheus Dias <matheusdiassaraiva@edu.unifor.br>
2022-04-10 18:33:51 -0300

    Merge remote-tracking branch 'origin/renatogonlop-V.2' into cyber

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
         <Route path="home" element={<Home />} />
         <Route path="faq" element={<Faq />} />
         <Route path="cadastro" element={<Cadastro/>} />
+        <Route path="/playlist/:id" element={<PlaylistList/>} />
       </Routes>
 
       <Footer />
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,59 +4,6 @@ function Cards() {
   const props = [
     {
       id: "1",
-      title__In: "a",
-      subtitle: "aa",
-      title__Out: "aaa",
-      description: "aaaa",
-    },
-    {
-      id: "2",
-      title__In: "b",
-      subtitle: "bb",
-      title__Out: "bbb",
-      description: "bbbb",
-    },
-    {
-      id: "3",
-      title__In: "c",
-      subtitle: "cc",
-      title__Out: "ccc",
-      description: "cccc",
-    },
-    {
-      id: "4",
-      title__In: "d",
-      subtitle: "dd",
-      title__Out: "ddd",
-      description: "dddd",
-    },
-    {
-      id: "5",
-      title__In: "e",
-      subtitle: "ee",
-      title__Out: "eee",
-      description: "eeee",
-    },
-    {
-        id: "6",
-        title__In: "a",
-        subtitle: "aa",
-        title__Out: "aaa",
-        description: "aaaa",
-      },
-      {
-        id: "7",
-        title__In: "a",
-        subtitle: "aa",
-        title__Out: "aaa",
-        description: "aaaa",
-      },
-      {
-        id: "8",
-        title__In: "a",
-        subtitle: "aa",
-        title__Out: "aaa",
-        description: "aaaa",
       image: "pato.png",
       title__In: "Zhu",
       subtitle: "Ringos Desert"
@@ -96,23 +43,25 @@ function Cards() {
         id: "8",
         title__In: "Aurora",
         subtitle: "The Gods We Can Touch"
-      },
+      }
   ];
-  return props.map((card) => (
-    <article className="card">
-      <div className="wrapper">
-        <div key={card.id} className="cover">
-        <div key={card.id} className="cover"><img id="back_capa" src={card.image}/>
-          <div className="cover__text">
-            <h1 className="cover__title">{card.title__In}</h1>
-            <h4 className="cover__subtitle">{card.subtitle}</h4>
+  return props.map(
+    (card) => (
+      <Link to={`/playlists/${p.id}`}>
+          <article className="card">
+            <div className="wrapper">
+              <div key={card.id} className="cover">
+              <img id="back_capa" src={card.image}/>
+                <div className="cover__text">
+                  <h1 className="cover__title">{card.title__In}</h1>                  <h4 className="cover__subtitle">{card.subtitle}</h4>
+                </div>
+              <div className="pb-wrapper">
+                <button className="play-button">Go</button>
+              </div>
+            </div>
           </div>
-          <div className="pb-wrapper">
-            <button className="play-button">Go</button>
-          </div>
-        </div>
-      </div>
-    </article>
+        </article>
+      </Link>
   ));
 }
 
